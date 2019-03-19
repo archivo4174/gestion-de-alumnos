1a731151371102355133a221e5b8e4bd70059529
Ignacio <ignacio@MacBook-Pro-de-BelugaMac.local>
2019-03-19 11:35:08 -0600

    configuracion firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDnwFeYci3AMs8EQ18AhEmVMJ6Wj7I8Bmk",
+    authDomain: "gestion-de-alumnos-cecy.firebaseapp.com",
+    databaseURL: "https://gestion-de-alumnos-cecy.firebaseio.com",
+    projectId: "gestion-de-alumnos-cecy",
+    storageBucket: "gestion-de-alumnos-cecy.appspot.com",
+    messagingSenderId: "384884157286"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDnwFeYci3AMs8EQ18AhEmVMJ6Wj7I8Bmk",
+    authDomain: "gestion-de-alumnos-cecy.firebaseapp.com",
+    databaseURL: "https://gestion-de-alumnos-cecy.firebaseio.com",
+    projectId: "gestion-de-alumnos-cecy",
+    storageBucket: "gestion-de-alumnos-cecy.appspot.com",
+    messagingSenderId: "384884157286"
+  }
 };
 
 /*
